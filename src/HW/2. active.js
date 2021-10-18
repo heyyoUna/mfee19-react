a4b8a9207409b9348db8cba04558ac95d2e96f48
Una Chang <una.c303@outlook.com>
2021-10-18 17:51:39 +0800

    active作業，把標籤button換成a，hook檢查抓不到

--- src/HW/2. active.js
+++ src/HW/2. active.js
@@ -1,25 +1,23 @@
-// 加上  e.target.classList.add('active')
-//就會報錯，不確定要怎麼調整...
-
-//把標籤button改成a，開檢查時hook又會抓不到
+//把標籤button改成a，開檢查時hook會抓不到
 
 import React, { useState } from 'react'
 import './App.scss'
 
 function App() {
   const [nav, setNav] = useState('')
-
   return (
-    <section id="activeHW">
-      <ul>
+    <>
+      <ul className="activeHW">
         <li>
           <button
-            href="/"
+            href="#/"
             type="button"
             value="首頁"
             checked={nav === '首頁'}
-            onClick={(e) => setNav(e.target.value)}
-            // e.target.classList.add('active')
+            onClick={(e) => {
+              e.target.classList.toggle('active')
+              setNav(e.target.value)
+            }}
           >
             首頁
           </button>
@@ -27,12 +25,14 @@ function App() {
 
         <li>
           <button
-            href="/"
+            href="#/"
             type="button"
             value="關於我們"
             checked={nav === '關於我們'}
-            onClick={(e) => setNav(e.target.value)}
-            // e.target.classList.add('active')
+            onClick={(e) => {
+              e.target.classList.toggle('active')
+              setNav(e.target.value)
+            }}
           >
             關於我們
           </button>
@@ -40,18 +40,20 @@ function App() {
 
         <li>
           <button
-            href="/"
+            href="#/"
             type="button"
             value="產品"
             checked={nav === '產品'}
-            onClick={(e) => setNav(e.target.value)}
-            // e.target.classList.add('active')
+            onClick={(e) => {
+              e.target.classList.toggle('active')
+              setNav(e.target.value)
+            }}
           >
             產品
           </button>
         </li>
       </ul>
-    </section>
+    </>
   )
 }
 export default App
