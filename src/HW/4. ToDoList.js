d42129db02d613e19a36d09745aef717fcfe4353
Una Chang <una.c303@outlook.com>
2021-10-18 18:08:16 +0800

    無法增加className

--- src/HW/4. ToDoList.js
+++ src/HW/4. ToDoList.js
@@ -1,13 +1,14 @@
-//出現錯誤 TypeError: toDoList.map is not a function，但不知道哪邊有寫錯QQ
+//第37, 38行出錯，TypeError: Cannot read properties of undefined (reading 'toggle')
+//換成add也一樣
 
 import React, { useState } from 'react'
 import './App.scss'
 
 function App() {
   const [inputTxt, setInputTxt] = useState('')
-  const [toDoList, setToDolist] = useState('')
+  const [toDoList, setToDolist] = useState([])
   return (
-    <>
+    <section>
       <h3>待辦清單</h3>
       <input
         type="text"
@@ -21,9 +22,13 @@ function App() {
       <button
         onClick={(e) => {
           e.preventDefault()
-          const newToDoList = [...toDoList, inputTxt] //把新增的工作，塞進列表內
-          setToDolist(newToDoList)
-          setInputTxt('') //送出後，將輸入框清空
+          if (inputTxt.length <= 0) { //沒內容不給送出
+            alert('請輸入內容')
+          } else {
+            const newToDoList = [...toDoList, inputTxt] //把新增的工作，塞進列表內
+            setToDolist(newToDoList)
+            setInputTxt('') //送出後，將輸入框清空
+          }
         }}
       >
         新增
@@ -33,15 +38,17 @@ function App() {
         return (
           <li
             key={i}
+            value={v}
             onClick={(e) => {
-              e.target.classlist.add('finished')
+              e.target.classlist.toggle('finished')
+              setToDolist(e.target)
             }}
           >
             {v}
           </li>
         )
       })}
-    </>
+    </section>
   )
 }
 export default App
